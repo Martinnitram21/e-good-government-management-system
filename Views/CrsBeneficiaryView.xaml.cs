799918c45d7c88934509eb6b075e9faed07721a0
itsmejay18 <jayjeminoababon@gmail.com>
2026-09-11 15:03:18 +0800

    Update CrsBeneficiaryView.xaml.cs

--- Views/CrsBeneficiaryView.xaml.cs
+++ Views/CrsBeneficiaryView.xaml.cs
@@ -1,11 +1,25 @@
+using System.Windows;
 using System.Windows.Controls;
+using GoodGovernanceApp.ViewModels;
 
 namespace GoodGovernanceApp.Views;
 
 public partial class CrsBeneficiaryView : UserControl
 {
+    private bool _initialLoadStarted;
+
     public CrsBeneficiaryView()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (_initialLoadStarted || DataContext is not CrsBeneficiaryViewModel viewModel)
+            return;
+
+        _initialLoadStarted = true;
+        await viewModel.LoadAsync();
     }
 }
